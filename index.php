99e1f941c30d2cd3a071f459d3d20ce27d44542a
Iván <jivanrod@gmail.com>
2023-10-23 14:21:13 +0200

    fix

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <table>
             <tr>
                 <td> round(0.50)</td>
-                <td class="conversion"><?php var_dump( round(0.50)); ?></td>
+                <td class="conversion"><?php var_dump(round(0.50)); ?></td>
             </tr>
             <tr>
                 <td>round(-4.60)</td>
@@ -23,11 +23,11 @@
             </tr>
             <tr>
                 <td>pow(2,4)</td>
-                <td class="conversion"><?php var_dump(pow(2,4)); ?></td>
+                <td class="conversion"><?php var_dump(pow(2, 4)); ?></td>
             </tr>
             <tr>
                 <td> intdiv(5, 2)</td>
-                <td class="conversion"><?php var_dump( intdiv(5, 2)); ?></td>
+                <td class="conversion"><?php var_dump(intdiv(5, 2)); ?></td>
             </tr>
             <tr>
                 <td>decbin("140")</td>
@@ -35,7 +35,7 @@
             </tr>
             <tr>
                 <td>ceil(0.40)</td>
-                <td class="conversion"><?php var_dump( ceil(0.40)); ?></td>
+                <td class="conversion"><?php var_dump(ceil(0.40)); ?></td>
             </tr>
             <tr>
                 <td>ceil(-5.1)</td>
@@ -65,80 +65,108 @@
                 <td class="conversion"><?php var_dump(floor(-5.9)); ?></td>
             </tr>
             <tr>
-                <td>chunk_split("Hello world!",1,".")</td>
-                <td class="conversion"><?php var_dump(chunk_split("Hello world!",1,".")); ?></td>
+                <td>abs(-5)</td>
+                <td class="conversion"><?php var_dump(abs(-5)); ?></td>
+            </tr>
+            <tr>
+                <td>cos(deg2rad(60))</td>
+                <td class="conversion"><?php var_dump(cos(deg2rad(60))); ?></td>
+            </tr>
+            <tr>
+                <td>max(5, 8, 3)</td>
+                <td class="conversion"><?php var_dump(max(5, 8, 3)); ?></td>
+            </tr>
+            <tr>
+                <td>chunk_split("Hola mundo!",1,".")</td>
+                <td class="conversion"><?php var_dump(chunk_split("Hola mundo!", 1, ".")); ?></td>
             </tr>
             <tr>
                 <td>chr(52)</td>
                 <td class="conversion"><?php var_dump(chr(52)); ?></td>
             </tr>
             <tr>
-                <td>count_chars("Hello World!", 3)</td>
-                <td class="conversion"><?php var_dump(count_chars("Hello World!", 3)); ?></td>
+                <td>count_chars("Hola World!", 3)</td>
+                <td class="conversion"><?php var_dump(count_chars("Hola World!", 3)); ?></td>
             </tr>
             <tr>
-                <td>count_chars("Hello World!", 1)</td>
-                <td class="conversion"><?php var_dump(count_chars("Hello World!", 1)); ?></td>
+                <td>count_chars("Hola World!", 1)</td>
+                <td class="conversion"><?php var_dump(count_chars("Hola World!", 1)); ?></td>
             </tr>
             <tr>
-                <td>explode(" ", "Hello world. It\'s a beautiful day.")</td>
-                <td class="conversion"><?php var_dump(explode(" ", "Hello world. It\'s a beautiful day.")); ?></td>
+                <td>explode(" ", "Hola mundo. It\'s a beautiful day.")</td>
+                <td class="conversion"><?php var_dump(explode(" ", "Hola mundo. It\'s a beautiful day.")); ?></td>
             </tr>
             <tr>
                 <td>htmlspecialchars("This is some <b>bold</b> text.")</td>
                 <td class="conversion"><?= htmlspecialchars("This is some <b>bold</b> text.") ?></td>
             </tr>
             <tr>
-                <td>ltrim("Hello World!", "Hello")</td>
-                <td class="conversion"><?php var_dump(ltrim("Hello World!", "Hello")); ?></td>
+                <td>ltrim("Hola World!", "Hola")</td>
+                <td class="conversion"><?php var_dump(ltrim("Hola World!", "Hola")); ?></td>
             </tr>
             <tr>
                 <td>ord("hello")</td>
                 <td class="conversion"><?php var_dump(ord("hello")); ?></td>
             </tr>
             <tr>
-                <td>str_repeat("Wow",13)</td>
-                <td class="conversion"><?php var_dump(str_repeat("Wow",13)); ?></td>
+                <td>str_repeat("Guau",13)</td>
+                <td class="conversion"><?php var_dump(str_repeat("Wow", 13)); ?></td>
             </tr>
             <tr>
-                <td>str_replace("world","Peter","Hello world!")</td>
-                <td class="conversion"><?php var_dump(str_replace("world","Peter","Hello world!")); ?></td>
+                <td>str_replace("mundo","tierra","Hola mundo!")</td>
+                <td class="conversion"><?php var_dump(str_replace("mundo", "tierra", "Hola mundo!")); ?></td>
             </tr>
             <tr>
-                <td>str_split("Hello")</td>
-                <td class="conversion"><?php var_dump(str_split("Hello")); ?></td>
+                <td>str_split("Hola")</td>
+                <td class="conversion"><?php var_dump(str_split("Hola")); ?></td>
             </tr>
             <tr>
-                <td>str_word_count("Hello world!")</td>
-                <td class="conversion"><?php var_dump(str_word_count("Hello world!")); ?></td>
+                <td>str_word_count("Hola mundo!")</td>
+                <td class="conversion"><?php var_dump(str_word_count("Hola mundo!")); ?></td>
             </tr>
             <tr>
-                <td>strcmp("Hello world!","Hello world!")</td>
-                <td class="conversion"><?php var_dump(strcmp("Hello world!","Hello world!")); ?></td>
+                <td>strcmp("Hola mundo!","Hola mundo!")</td>
+                <td class="conversion"><?php var_dump(strcmp("Hola mundo!", "Hola mundo!")); ?></td>
             </tr>
             <tr>
                 <td>strpos("I love php, I love php too!","php")</td>
-                <td class="conversion"><?php var_dump(strpos("I love php, I love php too!","php")); ?></td>
+                <td class="conversion"><?php var_dump(strpos("I love php, I love php too!", "php")); ?></td>
             </tr>
             <tr>
-                <td>strrev("Hello World!")</td>
-                <td class="conversion"><?php var_dump(strrev("Hello World!")); ?></td>
+                <td>strrev("Hola World!")</td>
+                <td class="conversion"><?php var_dump(strrev("Hola World!")); ?></td>
             </tr>
             <tr>
-                <td>strspn("Hello world!","kHlleo")</td>
-                <td class="conversion"><?php var_dump(strspn("Hello world!","kHlleo")); ?></td>
+                <td>str_pad("Hola", 10, "_", STR_PAD_BOTH)</td>
+                <td class="conversion"><?php var_dump(str_pad("Hola", 10, "_", STR_PAD_BOTH)); ?></td>
+            </tr>
+             <tr>
+                <td>str_contains('Hola', 'ol')</td>
+                <td class="conversion"><?php var_dump(str_contains('Hola', 'ol')); ?></td>
             </tr>
             <tr>
-                <td>strtoupper("Hello WORLD!")</td>
-                <td class="conversion"><?php var_dump(strtoupper("Hello WORLD!")); ?></td>
+                <td>str_shuffle("Hola")</td>
+                <td class="conversion"><?php var_dump(str_shuffle("Hola")); ?></td>
             </tr>
             <tr>
-                <td>substr("Hello world",6)</td>
-                <td class="conversion"><?php var_dump(substr("Hello world",6)); ?></td>
+                <td>strspn("Hola mundo!","kHlleo")</td>
+                <td class="conversion"><?php var_dump(strspn("Hola mundo!", "kHlleo")); ?></td>
             </tr>
             <tr>
-                <td>strlen("Hello")</td>
-                <td class="conversion"><?php var_dump(strlen("Hello")); ?></td>
+                <td>strtoupper("Hola WORLD!")</td>
+                <td class="conversion"><?php var_dump(strtoupper("Hola WORLD!")); ?></td>
+            </tr>
+            <tr>
+                <td>strtolower("Hola, Mundo!")</td>
+                <td class="conversion"><?php var_dump(strtolower("Hola, Mundo!")); ?></td>
+            </tr>
+            <tr>
+                <td>substr("Hola mundo",6)</td>
+                <td class="conversion"><?php var_dump(substr("Hola mundo", 6)); ?></td>
+            </tr>
+            <tr>
+                <td>strlen("Hola")</td>
+                <td class="conversion"><?php var_dump(strlen("Hola")); ?></td>
             </tr>
         </table>
     </body>
